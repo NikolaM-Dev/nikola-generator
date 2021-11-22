7a0ab8a372b4c9517e61db789ad0fda4eea7c74e
Juan David Merchan Torres <juan1014166@gmail.com>
2021-11-21 20:41:13 -0500

    feat(templates): Change react eslint config

--- templates/typescript/react/.eslintrc.js
+++ templates/typescript/react/.eslintrc.js
@@ -1,87 +1,82 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    'settings': {
-        'react': {
-          'version': 'detect'
-        }
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
       },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'prettier',
-        'import',
-
     ],
-    rules: {
-        'no-console': 'warn',
-        'prettier/prettier': [
-          'error',
-          {
-            printWidth: 80,
-            trailingComma: 'all',
-            tabWidth: 2,
-            semi: true,
-            singleQuote: true,
-            bracketSpacing: true,
-            arrowParens: 'always',
-            endOfLine: 'lf',
-          },
-        ],
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-              args: 'after-used',
-              ignoreRestSiblings: false,
-              argsIgnorePattern: '^_.*?$',
-            },
-          ],
-        'import/order': [
-          'warn',
-          {
-            'newlines-between': 'always',
-          },
-        ],
-        'react/prop-types': 'enabled',
-        'react/jsx-uses-vars': 'warn',
-        'react/jsx-uses-react': 'warn',
-        'react/self-closing-comp': 'warn',
-        'react/jsx-sort-props': [
-          'warn',
-          {
-            'callbacksLast': true,
-            'shorthandFirst': true,
-            'noSortAlphabetically': false,
-            'reservedFirst': true
-          }
-        ],
-        'padding-line-between-statements': [
-          'error',
-          { blankLine: 'always', prev: '*', next: 'return' },
-          { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-          {
-            blankLine: 'any',
-            prev: ['const', 'let', 'var'],
-            next: ['const', 'let', 'var'],
-          },
-        ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$',
       },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-uses-react': 'warn',
+    'react/self-closing-comp': 'warn',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
 };
