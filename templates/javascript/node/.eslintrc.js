05931778c72f332faaf219f83c3a16b0701140e0
Juan David Merchan Torres <juan1014166@gmail.com>
2021-11-21 20:03:29 -0500

    feat(templates): Add javascript templates

---
+++ templates/javascript/node/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['prettier', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$',
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
+};
---
+++ templates/javascript/react/.eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  },
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$',
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'react/prop-types': 'enabled',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-uses-react': 'warn',
+    'react/self-closing-comp': 'warn',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        'callbacksLast': true,
+        'shorthandFirst': true,
+        'noSortAlphabetically': false,
+        'reservedFirst': true
+      }
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
+};
