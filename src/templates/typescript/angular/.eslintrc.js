b294b25ee47123929b876c7285e7b37f4805d53a
Juan David Merchan Torres <juan1014166@gmail.com>
2022-01-10 14:34:31 -0500

    feat(templates): Add angular template

---
+++ src/templates/typescript/angular/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+		browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$',
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+  },
+};
